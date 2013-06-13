b9851d9705eeba88a1e9f67b38aa3c69dc5e7d59
creilly <reilly.christopher@gmail.com>
2013-06-13 17:48:06 -0500

    got rid of old stuff

--- clients/daqmxwebapp/background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create(
-      'daqclient.html',
-      {
-	  'width': 800,
-	  'height': 800
-      }
-  );
-});
--- libs/sitz.py
+++ libs/sitz.py
@@ -35,31 +35,8 @@ def compose(func_1, func_2):
         return func_1(func_2(*args, **kwargs))
     return composition
 
-#WORKAROUND FOR UNICODE ISSUE
-def _decode_list(data):
-    rv = []
-    for item in data:
-        if isinstance(item, unicode):
-            item = item.encode('utf-8')
-        elif isinstance(item, list):
-            item = _decode_list(item)
-        elif isinstance(item, dict):
-            item = _decode_dict(item)
-        rv.append(item)
-    return rv
-def _decode_dict(data):
-    rv = {}
-    for key, value in data.iteritems():
-        if isinstance(key, unicode):
-           key = key.encode('utf-8')
-        if isinstance(value, unicode):
-           value = value.encode('utf-8')
-        elif isinstance(value, list):
-           value = _decode_list(value)
-        elif isinstance(value, dict):
-           value = _decode_dict(value)
-        rv[key] = value
-    return rv
+=======
+class SitzException(Exception): pass
     
 TAG = '__tag__'
 def tagger(collectionName,tag=TAG):
