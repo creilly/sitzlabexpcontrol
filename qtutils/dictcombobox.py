6ed8419515ebdc0710a83da0b2c36c0d02e19e51
Bobby Stevens <stevens4@ph.utexas.edu>
2013-06-27 15:36:18 -0500

    added a combobox to process dictionaries

---
+++ qtutils/dictcombobox.py
@@ -0,0 +1,21 @@
+from PySide import QtGui, QtCore
+
+
+
+
+#by stevens4, given a dictionary, populates a combo out of the values and emits
+#the associated key via the choiceMade signal.
+class DictComboBox(QtGui.QComboBox):
+    choiceMade = QtCore.Signal(object)
+    def __init__(self,itemsDict):
+        #subclass combobox to pick from the values in dictionary and emit key
+        self.itemsDict = itemsDict
+        QtGui.QComboBox.__init__(self)
+        for item in self.itemsDict.values(): self.addItem(item)
+ 
+        def madeChoice(index):
+            self.choiceMade.emit(self.itemsDict.keys()[index])
+        
+        self.currentIndexChanged.connect(madeChoice)
+        
+ 
