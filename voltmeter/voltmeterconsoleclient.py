943d981ab8aa0172445048fe8c7eb3c931c29d1d
creilly <reilly.christopher@gmail.com>
2013-08-21 12:19:52 -0500

    modified voltmeter for channel editing and multiple plots

--- voltmeter/voltmeterconsoleclient.py
+++ voltmeter/voltmeterconsoleclient.py
@@ -1,15 +1,18 @@
 from ab.abclient import BaseClientFactory, BaseClientProtocol, getProtocol
 from ab.consoleclient import ConsoleClient, runConsoleClient, consoleCommand
-from ab.abbase import log, getDigit
+from ab.abbase import log, getDigit, selectFromList, getListIndex, getUserInput, getType
 from twisted.internet.defer import inlineCallbacks, returnValue, Deferred
 from twisted.internet import reactor
 from config.voltmeter import VM_SERVER_CONFIG, VM_DEBUG_SERVER_CONFIG
 from time import clock
+from daqmx.task.ai import AITask as AI
+from functools import partial
 
 #look for the user to pass 'debug' at runtime
-import sys
-DEBUG = len(sys.argv) > 1
 
+import sys
+
+DEBUG = len(sys.argv) > 1
 
 class VoltMeterClient(ConsoleClient):
     '''    
@@ -20,17 +23,65 @@ class VoltMeterClient(ConsoleClient):
     @consoleCommand('stop','stop the voltmeter')
     def stopVM(self):
         return self.protocol.sendCommand('stop')
-    '''
-        
+    '''        
     @consoleCommand('get voltages','gets the most recently acquired voltages')
     def getVoltages(self):
         return self.protocol.sendCommand('get-voltages')
 
+    @consoleCommand('get channel parameter','retrieves channel parameter')
+    @inlineCallbacks
+    def getChannelParameter(self):
+        channel, parameter = yield self.selectChannelParameter()
+        value = yield self.protocol.sendCommand('get-channel-parameter',channel,parameter)
+        returnValue(
+            zip(
+                *AI.TERMINAL_CONFIGS
+            )[2][
+                zip(
+                    *AI.TERMINAL_CONFIGS
+                )[0].index(value)
+            ] if parameter is AI.TERMINAL_CONFIG else value
+        )
+
+    @consoleCommand('set channel parameter','sets channel parameter')
+    @inlineCallbacks
+    def setChannelParameter(self):
+        channel, parameter = yield self.selectChannelParameter()
+        if parameter is AI.PHYSICAL_CHANNEL:
+            returnValue('not yet supported')
+        if parameter in (AI.MIN,AI.MAX):
+            value = yield getType(float,'enter voltage: ')
+        if parameter is AI.TERMINAL_CONFIG:            
+            trmCfgIndex = yield getListIndex(zip(*AI.TERMINAL_CONFIGS)[2],'select configuration')
+            value = zip(*self.TERMINAL_CONFIGS)[0][trmCfgIndex]
+        if parameter is AI.DESCRIPTION:
+            value = yield getUserInput('enter description: ')
+        yield self.protocol.sendCommand('set-channel-parameter',channel,parameter,value)
+
+    @inlineCallbacks
+    def selectChannelParameter(self):
+        channel = yield self.selectChannel()
+        parameter = yield self.selectParameter()
+        returnValue((channel,parameter))
+        
+    @inlineCallbacks
+    def selectChannel(self):
+        channels = yield self.protocol.sendCommand('get-channels')
+        channel = yield selectFromList(channels,'select channel')
+        returnValue(channel)
+        
+    @inlineCallbacks
+    def selectParameter(self):
+        paramIndex = yield getListIndex(zip(*AI.PARAMETERS)[1],'select parameter')
+        param = zip(*AI.PARAMETERS)[0][paramIndex]
+        returnValue(param)
+        
     @inlineCallbacks
     def initializeConsoleClient(self):
         url = VM_SERVER_CONFIG['url'] if not DEBUG else VM_DEBUG_SERVER_CONFIG['url']
         self.protocol = yield getProtocol(url)
-        yield self.protocol.messageSubscribe('voltages-measured',self.onVoltagesMeasured)
+        def onMessage(d): print 'message: %s' % d
+        self.protocol.messageSubscribe('channel-parameter-changed',onMessage)
 
     def onVoltagesMeasured(self,voltages):
         print clock()
--- voltmeter/voltmetergui.py
+++ voltmeter/voltmetergui.py
@@ -14,91 +14,342 @@ from sitz import compose
 from time import clock
 from ab.abbase import selectFromList, sleep
 from functools import partial
-from pyqtgraph import PlotWidget
+import pyqtgraph as pg
 import os.path
 from config.filecreation import POOHDATAPATH
 from filecreationmethods import filenameGen, checkPath
 import csv
 import datetime
+from daqmx.task.ai import VoltMeter as VM
 
 DEBUG = len(sys.argv) > 1 and sys.argv[1] == 'debug'
 URL = (VM_DEBUG_SERVER_CONFIG if DEBUG else VM_SERVER_CONFIG)['url']
 MAX = 200
 SLEEP = .1
 
-class VoltMeterWidget(QtGui.QWidget):
-    def __init__(self,protocol):
-        QtGui.QWidget.__init__(self)
-        self.setLayout(QtGui.QVBoxLayout())
-        plotter = PlotWidget()
-        self.plot = plotter.plot()
-        self.layout().addWidget(plotter,1)
-        self.filename = None
-        self.fileObj = None
-        
-        voltages = [0] * MAX
-        
-        def onVoltagesAcquired(data):
-            datum = data[self.channel] *1000
-            lcd.display(datum)
-            voltages.pop(0)
-            voltages.append(datum)
-            self.plot.setData(range(len(voltages)),voltages)
-            if self.filename is not None:
-                timeStamp = datetime.datetime.now() - self.startTime
-                timeStampStr = str(timeStamp.seconds)+'.'+str(timeStamp.microseconds/1000).zfill(3)
-                csvLine = timeStampStr+','+str(datum)+'\n'
-                self.fileObj.write(csvLine)
-
-
-        controlPanel = QtGui.QHBoxLayout()
-        controlPanel.addStretch(1)
-
-        vmCombo = QtGui.QComboBox()
-        vmCombo.currentIndexChanged[unicode].connect(
-            partial(setattr,self,'channel')
-        )
-        vmCombo.setCurrentIndex(0)
-        protocol.sendCommand('get-channels').addCallback(vmCombo.addItems)
-
-        controlPanel.addWidget(vmCombo)
-
-        def recButFunc():
-            #if filename isn't set, initialize a file and filewriter to write to
-            if self.filename == None:
-                vmName = vmCombo.currentText()
-                subfolder = os.path.join('voltmeterLog',vmName)
-                relPath, self.filename = filenameGen(subfolder)
-                absPath = os.path.join(POOHDATAPATH,relPath)
-                checkPath(absPath)
-                self.filename = os.path.join(absPath,self.filename+'.csv')
-                self.fileObj = open(self.filename, 'wb')
-                self.startTime = datetime.datetime.now()
-                recordButton.setText('logging...')
-            #if there is a filename, close the file and set filename to none
-            else:
-                self.filename = None
-                self.fileObj.close()
-                recordButton.setText('start log')
-                
-        recordButton = QtGui.QPushButton('log')
-        recordButton.clicked.connect(recButFunc)
-        controlPanel.addWidget(recordButton)
-
-        lcd = QtGui.QLCDNumber(5)
-        lcd.setSegmentStyle(lcd.Flat)
-
-        controlPanel.addWidget(lcd)
-
-        self.layout().addLayout(controlPanel)
-        
+####################
+#
+# WARNING:
+#
+# if the voltmeter is unable to
+# set a parameter to the requested
+# value, an error will print to
+# the server's log with no further
+# action. reload a fresh edit
+# dialog to see the current channel
+# parameter settings
+#
+####################
+class ChannelEditDialog(QtGui.QDialog):
+    colorChanged = QtCore.Signal(QtGui.QColor)
+    def __init__(self,protocol,channel,color,parent=None):
         @inlineCallbacks
-        def loop():
-            voltages = yield protocol.sendCommand('get-voltages')
-            onVoltagesAcquired(voltages)
-            yield sleep(SLEEP)
-            loop()
-        loop()
+        def init():
+            QtGui.QDialog.__init__(self,parent)
+            self.setWindowTitle('%s edit dialog' % channel)
+            layout = QtGui.QFormLayout()
+            self.setLayout(layout)
+            PARAM_KEYS,PARAM_NAMES = zip(*VM.PARAMETERS)
+            setParameter = partial(
+                protocol.sendCommand,
+                'set-channel-parameter',
+                channel
+            )
+            parameters = {}
+            for parameter in PARAM_KEYS:
+                value = yield protocol.sendCommand('get-channel-parameter',channel,parameter)
+                parameters[parameter] = value
+            layout.addRow(
+                PARAM_NAMES[
+                    PARAM_KEYS.index(
+                        VM.PHYSICAL_CHANNEL
+                    )
+                ],
+                QtGui.QLabel(parameters[VM.PHYSICAL_CHANNEL])
+            )
+            descriptionLineEdit = QtGui.QLineEdit(parameters[VM.DESCRIPTION])
+            descriptionLineEdit.returnPressed.connect(
+                compose(
+                    partial(
+                        setParameter,
+                        VM.DESCRIPTION
+                    ),
+                    descriptionLineEdit.text
+                )
+            )
+            layout.addRow(
+                PARAM_NAMES[
+                    PARAM_KEYS.index(
+                        VM.DESCRIPTION
+                    )
+                ],
+                descriptionLineEdit
+            )
+            trmCfgComboBox = QtGui.QComboBox()
+            for trmCfgKey, _, trmCfgName in VM.TERMINAL_CONFIGS:
+                trmCfgComboBox.addItem(
+                    trmCfgName,trmCfgKey
+                )
+            trmCfgComboBox.setCurrentIndex(
+                trmCfgComboBox.findData(
+                    parameters[
+                        VM.TERMINAL_CONFIG
+                    ]
+                )
+            )
+            trmCfgComboBox.currentIndexChanged.connect(
+                compose(
+                    partial(
+                        setParameter,
+                        VM.TERMINAL_CONFIG
+                    ),
+                    trmCfgComboBox.itemData
+                )
+            )
+            layout.addRow(
+                PARAM_NAMES[
+                    PARAM_KEYS.index(
+                        VM.TERMINAL_CONFIG
+                    )
+                ],
+                trmCfgComboBox
+            )
+            for param in (VM.MIN,VM.MAX):
+                spinBox = QtGui.QDoubleSpinBox()
+                spinBox.setDecimals(1)
+                spinBox.setRange(
+                    *{
+                        VM.MIN:(-10.0,-.1),
+                        VM.MAX:(.1,10.0)
+                    }[param]
+                )
+                spinBox.setValue(parameters[param])
+                spinBox.valueChanged.connect(
+                    partial(
+                        setParameter,
+                        param
+                    )
+                )
+                layout.addRow(
+                    PARAM_NAMES[
+                        PARAM_KEYS.index(
+                            param
+                        )
+                    ],
+                    spinBox
+                )
+            colorEditButton = QtGui.QPushButton('edit')
+            colorEditButton.pressed.connect(
+                compose(
+                    self.colorChanged.emit,
+                    partial(
+                        QtGui.QColorDialog.getColor,
+                        color
+                    )
+                )
+            )
+            layout.addRow('color',colorEditButton)
+        init()
+
+class VoltMeterWidget(QtGui.QWidget):
+    HUE_ROLE = 998
+    ID_ROLE = 999
+    HISTORY = 200
+    def __init__(self,protocol):
+        @inlineCallbacks
+        def init():
+            def rightClicked(listWidget,p):
+                item = listWidget.itemAt(p)
+                if item is None: return
+                channel = item.data(self.ID_ROLE)
+                editDialog = ChannelEditDialog(protocol,channel,colors[channel],self)
+                editDialog.colorChanged.connect(partial(colorChanged,channel))
+                editDialog.show()
+            def colorChanged(channel,color):
+                colors[channel] = color
+                items[channel].setBackground(color)
+                plots[channel].setPen(
+                    pg.mkPen(
+                        color,
+                        width=2
+                    )
+                )
+            @inlineCallbacks
+            def loop():
+                voltages = yield protocol.sendCommand('get-voltages')
+                for channel, voltage in voltages.items():
+                    xData, yData = data[channel]
+                    yData.pop(0)
+                    scale = yield protocol.sendCommand(
+                        'get-channel-parameter',
+                        channel,
+                        VM.MAX
+                    )
+                    yData.append(voltage)
+                    plots[channel].setData(
+                        xData,
+                        [100.0 * voltage / scale for voltage in yData]
+                    )
+                for item in (listWidget.item(row) for row in range(listWidget.count())):
+                    channel = item.data(self.ID_ROLE)
+                    if item.checkState() is QtCore.Qt.CheckState.Checked:
+                        if channel not in checked:
+                            checked.append(channel)
+                            plotWidget.addItem(
+                                plots[channel]
+                            )
+                    elif channel in checked:
+                        checked.remove(channel)
+                        plotWidget.removeItem(
+                            plots[channel]
+                        )
+                callbackRate = yield protocol.sendCommand('get-callback-rate')
+                yield sleep(1.0 / callbackRate)
+                loop()
+
+            QtGui.QWidget.__init__(self)
+            self.setLayout(QtGui.QHBoxLayout())
+            
+            plotWidget = pg.PlotWidget()
+            self.layout().addWidget(plotWidget,1)
+            
+            listWidget = QtGui.QListWidget()
+            listWidget.setDragDropMode(QtGui.QAbstractItemView.InternalMove)
+            listWidget.setContextMenuPolicy(
+                QtCore.Qt.ContextMenuPolicy.CustomContextMenu
+            )
+            listWidget.customContextMenuRequested.connect(
+                partial(
+                    rightClicked,
+                    listWidget
+                )
+            )
+            self.layout().addWidget(listWidget)            
+            channels = yield protocol.sendCommand('get-channels')
+            data = {
+                channel:(
+                    range(self.HISTORY),
+                    [0] * self.HISTORY
+                ) for index, channel in enumerate(channels)
+            }
+            colors = {
+                channel:QtGui.QColor.fromHsv(
+                    int(
+                        255 * float(index) / len(channels)
+                    ),
+                    255,
+                    255
+                ) for index, channel in enumerate(channels)
+            }
+            plots = {
+                channel:pg.PlotDataItem(
+                    data[channel][0],
+                    data[channel][1],
+                    name=channel,
+                    pen=pg.mkPen(
+                        colors[channel],
+                        width=2
+                    )
+                ) for channel in channels
+            }
+            items = {}
+            for index, channel in enumerate(channels):
+                description = yield protocol.sendCommand(
+                    'get-channel-parameter',
+                    channel,
+                    VM.DESCRIPTION
+                )
+                listWidgetItem = QtGui.QListWidgetItem()                
+                listWidgetItem.setData(self.ID_ROLE,channel)
+                listWidgetItem.setBackground(QtGui.QBrush(colors[channel]))
+                listWidgetItem.setText('%s\t(%s)' % (description,channel))
+                listWidget.addItem(listWidgetItem)
+                items[channel] = listWidgetItem
+            for index in range(listWidget.count()):
+                listWidget.item(index).setCheckState(QtCore.Qt.CheckState.Unchecked)                                             
+            def onChannelParameterChanged(channel,parameter,value):
+                if parameter is VM.DESCRIPTION:
+                    items[channel].setText('%s\t(%s)' % (value,channel))
+            protocol.messageSubscribe(
+                'channel-parameter-changed',
+                partial(
+                    apply,
+                    onChannelParameterChanged
+                )                
+            )
+            checked = []
+            loop()        
+        init()
+        
+        # plotter = PlotWidget()
+        # self.plot = plotter.plot()
+        # self.layout().addWidget(plotter,1)
+        # self.filename = None
+        # self.fileObj = None
+        
+        # voltages = [0] * MAX
+        
+        # def onVoltagesAcquired(data):
+        #     datum = data[self.channel] *1000
+        #     lcd.display(datum)
+        #     voltages.pop(0)
+        #     voltages.append(datum)
+        #     self.plot.setData(range(len(voltages)),voltages)
+        #     if self.filename is not None:
+        #         timeStamp = datetime.datetime.now() - self.startTime
+        #         timeStampStr = str(timeStamp.seconds)+'.'+str(timeStamp.microseconds/1000).zfill(3)
+        #         csvLine = timeStampStr+','+str(datum)+'\n'
+        #         self.fileObj.write(csvLine)
+
+
+        # controlPanel = QtGui.QHBoxLayout()
+        # controlPanel.addStretch(1)
+
+        # vmCombo = QtGui.QComboBox()
+        # vmCombo.currentIndexChanged[unicode].connect(
+        #     partial(setattr,self,'channel')
+        # )
+        # vmCombo.setCurrentIndex(0)
+        # protocol.sendCommand('get-channels').addCallback(vmCombo.addItems)
+
+        # controlPanel.addWidget(vmCombo)
+
+        # def recButFunc():
+        #     #if filename isn't set, initialize a file and filewriter to write to
+        #     if self.filename == None:
+        #         vmName = vmCombo.currentText()
+        #         subfolder = os.path.join('voltmeterLog',vmName)
+        #         relPath, self.filename = filenameGen(subfolder)
+        #         absPath = os.path.join(POOHDATAPATH,relPath)
+        #         checkPath(absPath)
+        #         self.filename = os.path.join(absPath,self.filename+'.csv')
+        #         self.fileObj = open(self.filename, 'wb')
+        #         self.startTime = datetime.datetime.now()
+        #         recordButton.setText('logging...')
+        #     #if there is a filename, close the file and set filename to none
+        #     else:
+        #         self.filename = None
+        #         self.fileObj.close()
+        #         recordButton.setText('start log')
+                
+        # recordButton = QtGui.QPushButton('log')
+        # recordButton.clicked.connect(recButFunc)
+        # controlPanel.addWidget(recordButton)
+
+        # lcd = QtGui.QLCDNumber(5)
+        # lcd.setSegmentStyle(lcd.Flat)
+
+        # controlPanel.addWidget(lcd)
+
+        # self.layout().addLayout(controlPanel)
+        
+        # @inlineCallbacks
+        # def loop():
+        #     voltages = yield protocol.sendCommand('get-voltages')
+        #     onVoltagesAcquired(voltages)
+        #     yield sleep(SLEEP)
+        #     loop()
+        # loop()
 
 @inlineCallbacks
 def main():
