e12773cbf0d924d3f06e618d85409b27a8afba60
Bobby Stevens <stevens4@ph.utexas.edu>
2013-10-09 15:28:27 -0500

    created lidrotator.py and config file. this version is gui only and needs steppermotor code.

---
+++ config/lid.py
@@ -0,0 +1,17 @@
+'''
+this is our config file for the lid.
+
+it is a simple dictionary of 4 named positions of
+the lid and the counter values for each
+
+values are from sheng's config.txt on pooh's D:\forlab\rotator\
+'''
+
+
+LID_POSITIONS = {
+    'scatter':193600,
+    'sputter':100000,
+    'LEED':53000,
+    'LIPD':5000
+    }
+    
