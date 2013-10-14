9e966958392ed31ac5227e825e4ebf7d9bf1e1a2
Bobby Stevens <stevens4@ph.utexas.edu>
2013-10-14 10:31:17 -0500

    completed lidrotator app and updates to steppermotors to allow for DIO driving of a sm

--- config/lid.py
+++ config/lid.py
@@ -13,5 +13,19 @@ LID_POSITIONS = {
     'sputter':100000,
     'LEED':53000,
     'LIPD':5000
-    }
-    
+}
+ 
+DEBUG_LID_CONFIG = {
+    'logfile':'debuglidlog.txt',
+    'relay_channel':'alpha/port0/line3'
+}
+
+LID_CONFIG = {
+    'logfile':'lidlog.txt',
+    'relay_channel':'dev3/port0/line3',
+    'step_channel':'dev3/port0/line7',
+    'counter_channel':'dev3/ctr6',
+    'direction_channel':'dev3/port0/line5',
+    'step_rate':500.0,
+    'backlash':1525
+}
