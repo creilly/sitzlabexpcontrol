aac86a61552833632427c5a497213970feabc7b4
Bobby Stevens <stevens4@ph.utexas.edu>
2015-03-20 12:16:05 -0500

    updated default times for lasers

--- config/delaygenerator.py
+++ config/delaygenerator.py
@@ -49,7 +49,7 @@ DG_CONFIG = {
         },
     MAV_PUMP_LAMP:{
         'ard_id':'5533834353935150E1D1',  #COM4
-        'delay': 969450,      #3607400,
+        'delay': 2962450,      #3607400,
         'partner':MAV_PUMP_QSW,
         'rel_part_delay':227550.,
         'run_by_default':True,
@@ -60,7 +60,7 @@ DG_CONFIG = {
         },
     MAV_PUMP_QSW:{
         'ard_id': '5533834353935120E1D0',  #COM5
-        'delay':1197000, #3834950,
+        'delay':3190000, #3834950,
         'partner':MAV_PUMP_LAMP,
         'rel_part_delay':-227550.,
         'run_by_default':True,
@@ -71,7 +71,7 @@ DG_CONFIG = {
         },
     MAV_PROBE_LAMP:{
         'ard_id': '55330343731351C0A1B1',  #COM6
-        'delay':965000, #3604150,
+        'delay':2957600, #3604150,
         'partner':MAV_PROBE_QSW,
         'rel_part_delay':232400.,
         'run_by_default':True,
@@ -82,7 +82,7 @@ DG_CONFIG = {
         },
     MAV_PROBE_QSW:{
         'ard_id':'55330343731351906041',  #COM7
-        'delay':1197400, #3836550,
+        'delay':3190000, #3836550,
         'partner':MAV_PROBE_LAMP,
         'rel_part_delay':-232400.,
         'run_by_default':True,
